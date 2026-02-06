ce058469a0845f445498eec4f0ec83dc69da45a1
blue101010 <no@email.com>
2026-02-05 22:41:07 -0500

    fix pip and init for chromadb

---
+++ main.py
@@ -0,0 +1,18 @@
+import chromadb
+import uuid
+
+client = chromadb.Client()
+
+collection = client.create_collection(name="policies")
+
+with open("policy.txt", "r", encoding="utf-8") as f:
+    policies: list[str] = f.read().splitlines()
+
+
+collection.add(
+    ids=[str(uuid.uuid4()) for _ in policies],
+    documents=policies,
+    metadatas=[{"line": line} for line in range(len(policies))],
+)
+
+collection.peek()
